1353a243417011855e14675308d8f8d65c8d2a5b
Ruxi1 <ruxandramaria.baros@ulbsibiu.ro>
2024-03-24 10:11:20 +0200

    Add project files.

---
+++ AnimalSound/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSound
+{
+    public class Animal
+    {
+        private string name;
+        private string sound;
+
+        public Animal(string name, string sound)
+        {
+            this.name = name;
+            this.sound = sound;
+        }
+        public string GetName(string name)
+        {
+            return name;
+        }
+        public string GetSound(string sound)
+        {
+            return sound;
+        }
+
+        public void MakesSound()
+        {
+            Console.WriteLine("{0} makes sound: {1}",this.name,this.sound);
+        }
+    }
+}
---
+++ AnimalSound/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalSound
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+           
+            animals.Add(new Animal("duck", "mack"));
+            animals.Add(new Animal("bee", "buzz"));
+            animals.Add(new Animal("cat", "meow"));
+            animals.Add(new Animal("dog", "ham"));
+
+            for (int i = 0; i < animals.Count; i++)
+            {
+                animals[i].MakesSound();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
